ba27fddf62bf088b939c750177c294edcbe32e13
LiaoZike <99531832+LiaoZike@users.noreply.github.com>
2023-07-08 16:42:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 //手機板選單
 var button = document.querySelector('.aboutme');
